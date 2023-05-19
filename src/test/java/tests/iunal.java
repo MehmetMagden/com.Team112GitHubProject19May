94c479ca1070184877ca15c405c6c5a8a628b88e
Ibrahim Unal <125129575+iunal@users.noreply.github.com>
2023-05-19 21:13:39 +0200

    secondCommit

--- src/test/java/tests/iunal.java
+++ src/test/java/tests/iunal.java
@@ -4,6 +4,7 @@ public class iunal {
     public static void main(String[] args) {
 
         System.out.println("This class has been created by IUNAL");
+
         System.out.println("Second line of my class");
     }
 }
