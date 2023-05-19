e1175ae382f0840af6078075dca78b2b7f7e5e39
Mehmet Maden <yasamtr@gmail.com>
2023-05-19 20:23:07 +0300

    anyMessage

---
+++ src/test/java/tests/Mehmet.java
@@ -0,0 +1,36 @@
+package tests;
+
+public class Mehmet {
+
+    /*
+    1) create .gitignore file
+    # you have to create ".ignore" file before initialising gitHub by "git init" command
+    # we have written all folder names that we don't want to upload to gitHub remote server to prevent conflicts
+
+    /.idea/
+    /target/
+    /test-output/
+    /xmlFiles/
+
+    2) create packages pages, tests, utilities
+    copy utilities classes from the project
+
+    3) add dependencies testNG, selenium Java, webdrivermnanager , com.aventstack
+
+    ** so we have the testNG framework
+
+    let's create a copy of this project in gitHub
+
+    1) create a new repostory in gitHub
+        i) right top + button >>> new repostory
+        ii) name of the project com.Team112GitHubProject19May
+        iii) create
+
+    2) set git in your project in terminal
+    git init
+    git add .
+    git commit -m "firstCommit"   // you can write any message here instead of firstcommit
+
+
+     */
+}
