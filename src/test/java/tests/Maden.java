2e3ca665fde3e5dff7d44e38d10893ce2341b152
Ayse Yesiloglu <125242822+AyseYesiloglu@users.noreply.github.com>
2023-05-19 22:45:28 +0300

    hey

--- src/test/java/tests/Maden.java
+++ src/test/java/tests/Maden.java
@@ -5,6 +5,10 @@ public class Maden {
     public static void main(String[] args) {
 
         System.out.println("Hello word Maden");
+
+
+
+        System.out.println("conflict çözmek bizim işimiz");
     }
 
 }
