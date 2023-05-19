64117030cd35c50ca8560be738594677f8e7467e
ElcinCebeciKinsiz <128594772+ElcinCebeciKinsiz@users.noreply.github.com>
2023-05-19 20:48:21 +0100

    degisiklik

--- src/test/java/tests/Maden.java
+++ src/test/java/tests/Maden.java
@@ -5,6 +5,11 @@ public class Maden {
     public static void main(String[] args) {
 
         System.out.println("Hello word Maden");
+
+
+
+        System.out.println("Hep problem hep problem");
+
     }
 
 }
